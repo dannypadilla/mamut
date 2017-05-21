6790ff5280d32d14e102cca215e17f65c639ae58
Danny Padilla <dpadilla010@gmail.com>
2017-05-21 14:04:30 -0700

    create server template

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-// test
+var express = require("express");
+var app = express();
+var mongojs = require("mongojs");
+var db = mongojs("mamut", ["mamut"] ); // put database name here
+var bodyParser = require("body-parser");
+
+app.use(express.static(__dirname + "/public") );
+app.use(bodyParser.json() );
+
+app.get("/", function(req, res) { // needs more setup
+    console.log("Received GET request");
+});
+
+
+app.listen(3000);
+console.log("Server running on port 3000");
